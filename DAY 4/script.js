8394fe8f56b6e0e2aa682cdde5c509e05b728a71
Garima <garimapathania23@gmail.com>
2026-06-25 11:30:41 +0530

    JavaScript Started

---
+++ DAY 4/script.js
@@ -0,0 +1,37 @@
+// let num1 = 12;
+let num2 = 15;
+// console.log("Number: ",num1);
+// // ${variable_name} --> Interpolation
+// console.log(`Number 1 is ${num1} \nNumber 2 is ${num2}\n Sum: ${num1+num2}`);
+// // console.log("Number 1 is",num1,"\nNumber 2 is",num2,"\nSum: ",num1 + num2);
+// // Declare
+// let num3;
+// // Initiallizing
+// num3 = 10;
+// // Declaring and Initiallizing
+// let num4 = 10;
+// num4 = 25;
+// console.log(num4);
+// const PI = 3.14;
+// var num5 = 10;
+// var API_KEY = "1234567890akadvcniuwEG"
+// var API_KEY = "sdr3wg45t4rgwefgqer"
+
+// let API2 = "efdffge"
+// let API2 = "12345"
+// let cannot be redeclared and var can be redeclared so we use var
+
+// Temporal Dead Zonethe period between when let and const is hoisted in the same memory and when the execution exists 
+// but not
+// console.log(num1);
+// var num1 = 10;
+// console.log(num1);
+
+// "Use Strict"
+var num1 = 10; // will create a variable
+num2 = 20; // create a property on the global object(window)
+
+
+
+let name = prompt("Enter your name");
+console.log(name);
